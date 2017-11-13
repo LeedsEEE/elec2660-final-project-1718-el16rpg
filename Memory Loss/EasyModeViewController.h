52fa1050e1613884b5b0d7cd0039fdac2fc8dd6e
Rhun Gwilym <rhungwilym@rhuns-macbook-pro.local>
2017-11-13 17:11:40 +0000

    Added View Controller classes for the different modes

---
+++ Memory Loss/EasyModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EasyModeViewController.h
+//  Memory Loss
+//
+//  Created by Rhun Gwilym on 13/11/2017.
+//  Copyright © 2017 Rhun Gwilym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EasyModeViewController : UIViewController
+
+@end
---
+++ Memory Loss/EasyModeViewController.m
@@ -0,0 +1,37 @@
+//
+//  EasyModeViewController.m
+//  Memory Loss
+//
+//  Created by Rhun Gwilym on 13/11/2017.
+//  Copyright © 2017 Rhun Gwilym. All rights reserved.
+//
+
+#import "EasyModeViewController.h"
+
+@interface EasyModeViewController ()
+
+@end
+
+@implementation EasyModeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
