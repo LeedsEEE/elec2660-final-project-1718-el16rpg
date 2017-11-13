3410621c3ff5ecbb1d9a1a0c71ed2ae56da67dd4
Rhun Gwilym <rhungwilym@rhuns-macbook-pro.local>
2017-11-13 17:02:56 +0000

    Initial Commit

---
+++ Memory Loss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memory Loss
+//
+//  Created by Rhun Gwilym on 13/11/2017.
+//  Copyright © 2017 Rhun Gwilym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Memory Loss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memory Loss
+//
+//  Created by Rhun Gwilym on 13/11/2017.
+//  Copyright © 2017 Rhun Gwilym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
