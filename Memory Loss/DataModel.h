1885f8438e441d1ce972e0cb2e8367581c5073fb
Rhun Gwilym <rhungwilym@rhuns-mbp>
2017-11-16 21:26:03 +0000

    Have created a Data Model class

---
+++ Memory Loss/DataModel.h
@@ -0,0 +1,13 @@
+//
+//  DataModel.h
+//  Memory Loss
+//
+//  Created by Rhun Gwilym on 16/11/2017.
+//  Copyright © 2017 Rhun Gwilym. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataModel : NSObject
+
+@end
---
+++ Memory Loss/DataModel.m
@@ -0,0 +1,13 @@
+//
+//  DataModel.m
+//  Memory Loss
+//
+//  Created by Rhun Gwilym on 16/11/2017.
+//  Copyright © 2017 Rhun Gwilym. All rights reserved.
+//
+
+#import "DataModel.h"
+
+@implementation DataModel
+
+@end
